1ea829407b4bbd7613f78f03a6c2dfac08fa012d
panos1998 <petridisp@ece.auth.gr>
2021-12-07 17:21:46 +0200

    Plots

--- Voice_Capture.py
+++ Voice_Capture.py
@@ -2,13 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sounddevice as sd
 
-Fs = 8000
-duration = 5
-voice = sd.rec(frames=duration * Fs,samplerate=Fs,channels=1,dtype='int16')
-sd.wait()
-time = np.linspace(0, len(voice - 1) / Fs, len(voice - 1))
-print(time, len(time))
-plt.plot(voice)
+Fs = 8000  # Sampling frequency
+duration = 5  # Recording duration in seconds
+voice = sd.rec(frames=duration * Fs, samplerate=Fs, channels=1, dtype='int16')  # Capture the voice
+# frames indicate  indirectly the duration of record, dtype is 16 bits per sample.
+sd.wait()  # close after recording finish
+time = np.linspace(0, len(voice - 1) / Fs, len(voice - 1))  # split x axis in voice-1 points
+print(voice)  # points have 1/Fs distance each other
+plt.plot(time, voice)  # plot in seconds
+plt.title("Seconds")
+plt.xlabel("Seconds")
+plt.ylabel("Voice amplitude")
 plt.show()
-plt.plot(time, voice)
+plt.plot((10**3)*time, voice)  # plot in milliseconds
+plt.title("Milliseconds")
+plt.xlabel("Milliseconds")
+plt.ylabel("Voice amplitude")
 plt.show()

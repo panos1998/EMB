c83e4deb970c5a60e2ffe004a1a47de7df985af6
panos1998 <petridisp@ece.auth.gr>
2021-12-10 17:19:14 +0200

    add blocking argument

--- MainApp.py
+++ MainApp.py
@@ -4,7 +4,7 @@ import sounddevice as sd
 
 Fs = 8000  # Sampling frequency
 duration = 5  # Recording duration in seconds
-voice = sd.rec(frames=duration * Fs, samplerate=Fs, channels=1, dtype='int16')  # Capture the voice
+voice = sd.rec(frames=duration * Fs, blocking=True, samplerate=Fs, channels=1, dtype='int16')  # Capture the voice
 # frames indicate  indirectly the duration of record, dtype is 16 bits per sample.
 vplt.time_plot(voice, Fs)
 vplt.fourier_plot(voice, Fs)

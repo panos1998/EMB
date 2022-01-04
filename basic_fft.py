e0acd847552cf2b667281419486938903db4e95b
panos1998 <petridisp@ece.auth.gr>
2022-01-05 01:28:17 +0200

    better readability

--- basic_fft.py
+++ basic_fft.py
@@ -8,8 +8,9 @@ from scipy.fft import fft, fftfreq
 N = 600
 # sample spacing
 T = 1.0 / 800.0
-frequency_points = np.linspace(0.0, N*T, N, endpoint=False)
-sine_values = np.sin(50.0 * 2.0*np.pi*frequency_points) + np.sin(300*2*np.pi*frequency_points) + 0.5*np.sin(80.0 * 2.0*np.pi *frequency_points)
+freqs = np.linspace(0.0, N*T, N, endpoint=False)
+sine_values = np.sin(50.0 * 2.0*np.pi*freqs) + np.sin(300*2*np.pi*freqs) + \
+              0.5*np.sin(80.0 * 2.0*np.pi *freqs)
 fourier_values = fft(sine_values)
 frequency_values = fftfreq(N, T)[:N//2]
 plt.plot(frequency_values, 2.0/N * np.abs(fourier_values[0:N//2]))

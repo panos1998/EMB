c361bde8abbaa3cf81c14e5e6a6a41482f74fa8c
panos1998 <petridisp@ece.auth.gr>
2022-01-05 00:30:52 +0200

    maintain readability

--- VoicePlots.py
+++ VoicePlots.py
@@ -2,33 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.fft
 from scipy import signal
+from typing import List
 
 # plot signal in time domain
 
 def time_plot(data, frequency):
-    N = len(data) # get signal size
-    time = np.linspace(0, N / frequency, N)  # split x axis in voice-1 points
+    data_points_size = len(data) # get signal size
+    time = np.linspace(0, data_points_size / frequency, data_points_size)  # split x axis in voice-1 points
     # points have 1/Fs distance each other
     fig, ((axs2, axs4),(axs3, axs1)) = plt.subplots(2, 2)  # create subplots and create the given diagramms
     fig.suptitle('Time domain and sample domain plots')
-    axs1.plot(data / N)
+    axs1.plot(data / data_points_size)
     axs1.label_outer()
     axs1.set_xlabel('No of sample')
     axs2.set_ylabel('Voice amplitude')
-    axs2.plot(time, data / N)  # plot in seconds
-    axs3.plot((10**3)*time, data / N)  # plot in milliseconds
+    axs2.plot(time, data / data_points_size)  # plot in seconds
+    axs3.plot((10**3)*time, data / data_points_size)  # plot in milliseconds
     axs3.set_ylabel('Voice Amplitude')
     axs3.set_xlabel("Time")
     fig.delaxes(axs4)
     fig.show()
 
 def fourier_plot(data, frequency):
-    N = len(data)  # get data size
+    number_of_samples = len(data)  # get data size
     data = data.flatten()  # get appropriate input
     # Fourier transform
-    F = scipy.fft.fft(data) / N  # calculate fourier transform
-    f = np.linspace(0, frequency, N//2)  # split frequencies axis
-    plt.plot(f, abs(F[0:N // 2]))  # plot the fourier for non negative frequencies (first half of signal)
+    F = scipy.fft.fft(data) / number_of_samples  # calculate fourier transform
+    f = np.linspace(0, frequency, number_of_samples//2)  # split frequencies axis
+    plt.plot(f, abs(F[0:number_of_samples // 2]))  # plot the fourier for non negative frequencies (first half of signal)
     plt.title("FFT of the signal")
     plt.xlabel('Frequency')
     plt.ylabel('Power of Frequency')
@@ -38,7 +39,7 @@ def fourier_plot(data, frequency):
     # Also i has very high frequency but too low magnitude
 
 # function to plot a spectrogramm
-def spectrogram_plot(data, frequency):
+def spectrogram_plot(data: List[List], frequency: int) -> List:
     data = data.flatten()  # take the signal data
     freq, t, stft = signal.spectrogram(data, frequency, mode='complex')  # extract frequencies,
     # time and stfourrier
--- basic_fft.py
+++ basic_fft.py
@@ -5,10 +5,10 @@ from scipy.fft import fft, fftfreq
 N = 600
 # sample spacing
 T = 1.0 / 800.0
-x = np.linspace(0.0, N*T, N, endpoint=False)
-y = np.sin(50.0 * 2.0*np.pi*x) + np.sin(300*2*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
-yf = fft(y)
-xf = fftfreq(N, T)[:N//2]
-plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+frequency_axis = np.linspace(0.0, N*T, N, endpoint=False)
+sine_wave = np.sin(50.0 * 2.0*np.pi*frequency_axis) + np.sin(300*2*np.pi*frequency_axis) + 0.5*np.sin(80.0 * 2.0*np.pi *frequency_axis)
+sine_fourier = fft(sine_wave)
+frequency_partition = fftfreq(N, T)[:N//2]
+plt.plot(frequency_partition, 2.0/N * np.abs(sine_fourier[0:N//2]))
 plt.grid()
 plt.show()

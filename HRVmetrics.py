accc3e1debb6bb52cc07275145c66efe6be8ac80
panos1998 <petridisp@ece.auth.gr>
2022-01-05 02:20:18 +0200

    better readability

--- HRVmetrics.py
+++ HRVmetrics.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 import scipy.io
 from ecgdetectors import Detectors
 import hrv
-dict_data = scipy.io.loadmat(r"C:\Users\Πανος\Documents\GitHub\EMB\MLII\17 PR\107m (0).mat")
-data = dict_data['val'][0]
-Fs = 360  # Sampling Frequency in Hz
-detectors = Detectors(Fs)
-r_peaks = detectors.pan_tompkins_detector(data)
-Hr = hrv.HRV(360)  # Initialize a hrv.HRV class with sampling frequency
-heart_rate_variability = Hr.HR(rr_samples=r_peaks)
+dictdata = scipy.io.loadmat(r"C:\Users\Πανος\Documents\GitHub\EMB\MLII\17 PR\107m (0).mat")
+data = dictdata['val'][0]
+FREQ = 360  # Sampling Frequency in Hz
+detectors = Detectors(FREQ)
+rpeaks = detectors.pan_tompkins_detector(data)
+hr = hrv.HRV(360)  # Initialize a hrv.HRV class with sampling frequency
+heart_rate_variability = hr.HR(rr_samples=rpeaks)
 print(heart_rate_variability)
 plt.figure()
 plt.plot(data)
-plt.plot(r_peaks, data[r_peaks], 'ro')
+plt.plot(rpeaks, data[rpeaks], 'ro')
 plt.show()
 plt.title('Heart rate variability')
 plt.ylabel('Interval Seconds')

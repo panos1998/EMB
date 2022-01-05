2f0e7804fcacff20e331bb9e51644abfd969322e
panos1998 <petridisp@ece.auth.gr>
2022-01-05 02:05:50 +0200

    better readability

--- third_practice.py
+++ third_practice.py
@@ -6,16 +6,16 @@ from data_processing import data_formatting
 from time_plot import time_plot
 
 # extract formatted signal list
-signal_list, Fs = data_formatting(r'voice_data.mat')
+signal_list, FREQUENCY = data_formatting(r'voice_data.mat')
 # and sampling frequency
 i = 0 # index iterator
 # plot all signals in time domain
 for signal in signal_list:
-    time_plot(signal, Fs, index=i)  # plot each signal
+    time_plot(signal, FREQUENCY, index=i)  # plot each signal
     i += 1
 
 i = 0 # index iterator
 # plot all fourier transforms in frequency domain
 for signal in signal_list:
-    fourier_plot(signal, Fs, index=i)
+    fourier_plot(signal, FREQUENCY, index=i)
     i += 1

95ecd60f8914b9675b1a282511b6c0fbebb98a0a
panos1998 <petridisp@ece.auth.gr>
2022-01-05 02:28:37 +0200

    better readability

--- MainApp.py
+++ MainApp.py
@@ -5,10 +5,10 @@ import VoicePlots as vplt
 import sounddevice as sd
 
 FREQ = 8000  # Sampling frequency
-duration = 5  # Recording duration in seconds
+DURATION = 5  # Recording duration in seconds
 # Capture the voice
-voice = sd.rec(frames=duration * FREQ, blocking=True, samplerate=FREQ, channels=1, dtype='int16')
+points = sd.rec(frames=DURATION * FREQ, blocking=True, samplerate=FREQ, channels=1, dtype='int16')
 # frames indicate  indirectly the duration of record, dtype is 16 bits per sample.
-vplt.time_plot(voice, FREQ)
-vplt.fourier_plot(voice, FREQ)
-vplt.spectrogram_plot(voice, FREQ)
+vplt.time_plot(points, FREQ)
+vplt.fourier_plot(points, FREQ)
+vplt.spectrogram_plot(points, FREQ)

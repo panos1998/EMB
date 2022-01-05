1d35b3462117b0925f18b3731791a212e97673d8
panos1998 <petridisp@ece.auth.gr>
2022-01-05 02:22:46 +0200

    better readability

--- HRVmetrics.py
+++ HRVmetrics.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 import scipy.io
 from ecgdetectors import Detectors
 import hrv
-dictdata = scipy.io.loadmat(r"C:\Users\Πανος\Documents\GitHub\EMB\MLII\17 PR\107m (0).mat")
-data = dictdata['val'][0]
+matrixdata = scipy.io.loadmat(r"C:\Users\Πανος\Documents\GitHub\EMB\MLII\17 PR\107m (0).mat")
+data = matrixdata['val'][0]
 FREQ = 360  # Sampling Frequency in Hz
-detectors = Detectors(FREQ)
-rpeaks = detectors.pan_tompkins_detector(data)
+detector = Detectors(FREQ)
+rpeaks = detector.pan_tompkins_detector(data)
 hr = hrv.HRV(360)  # Initialize a hrv.HRV class with sampling frequency
-heart_rate_variability = hr.HR(rr_samples=rpeaks)
-print(heart_rate_variability)
+heartratevariability = hr.HR(rr_samples=rpeaks)
+print(heartratevariability)
 plt.figure()
 plt.plot(data)
 plt.plot(rpeaks, data[rpeaks], 'ro')
@@ -20,5 +20,5 @@ plt.show()
 plt.title('Heart rate variability')
 plt.ylabel('Interval Seconds')
 plt.xlabel('Sample')
-plt.plot(heart_rate_variability)
+plt.plot(heartratevariability)
 plt.show()

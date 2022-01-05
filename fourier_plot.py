d810c482a6e70752bb56b19a9c565ba982aa5046
panos1998 <petridisp@ece.auth.gr>
2022-01-05 02:34:30 +0200

    better readability

--- fourier_plot.py
+++ fourier_plot.py
@@ -6,8 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fft as fft
 
-fouriertransform = []  # initialize fourier list
-frequencyList = []  # initialize frequencies list
+fourier_values = []  # initialize fourier list
+frequencies = []  # initialize frequencies list
 # function to plot a list of fourrier transform in frequency domain
 def fourier_plot(signal_data: List, frequency: int, index: int) -> None:
     """
@@ -17,11 +17,11 @@ def fourier_plot(signal_data: List, frequency: int, index: int) -> None:
     :return:
     """
     # calculate fourier transform of the input signal
-    fouriertransform.append(fft.fft(signal_data))
+    fourier_values.append(fft.fft(signal_data))
     # calculate the corresponding fourier magnitude
-    fourier_mag = abs(fouriertransform[index])  # of all coeffs
-    frequencyList.append(np.linspace(0, frequency, len(signal_data) // 2)) # split frequencies axis
-    plt.plot(frequencyList[index], fourier_mag[0:len(signal_data) // 2]) # plot the data
+    fourier_mag = abs(fourier_values[index])  # of all coeffs
+    frequencies.append(np.linspace(0, frequency, len(signal_data) // 2)) # split frequencies axis
+    plt.plot(frequencies[index], fourier_mag[0:len(signal_data) // 2]) # plot the data
     plt.title(f'Fourrier of signal: {index + 1}')
     plt.xlabel('frequency in Hz')
     plt.ylabel('Fourier magnitude ')
--- third_practice.py
+++ third_practice.py
@@ -6,16 +6,16 @@ from data_processing import data_formatting
 from time_plot import time_plot
 
 # extract formatted signal list
-signaldata, FREQUENCY = data_formatting(r'voice_data.mat')
+data, FREQUENCY = data_formatting(r'voice_data.mat')
 # and sampling frequency
 i = 0 # index iterator
 # plot all signals in time domain
-for signal in signaldata:
+for signal in data:
     time_plot(signal, FREQUENCY, index=i)  # plot each signal
     i += 1
 
 i = 0 # index iterator
 # plot all fourier transforms in frequency domain
-for signal in signaldata:
+for signal in data:
     fourier_plot(signal, FREQUENCY, index=i)
     i += 1
